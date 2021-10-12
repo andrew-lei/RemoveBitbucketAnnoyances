af20628eda06c7f7e1d15ea44e01e3a3d26ae0c0
Andrew Lei <andrew.w.lei@gmail.com>
2021-10-11 23:25:37 -0700

    work on branch comparison page

--- content_script.js
+++ content_script.js
@@ -1,10 +1,16 @@
-const revisionSelector = document.getElementById('repository-layout-revision-selector');
-if (revisionSelector) {
+const isBitbucket = !!document.getElementsByClassName('bitbucket-theme');
+const branchDialogMap = new Map(
+    [['inline-dialog-repository-layout-revision-selector-dialog', 'inline-dialog-repository-layout-revision-selector-dialog-search-input']
+    ,['inline-dialog-sourceBranch-dialog', 'sourceBranchDialog-search-input']
+    ,['inline-dialog-targetBranch-dialog', 'targetBranchDialog-search-input']
+    ]);
+if (isBitbucket) {
     const config = { childList: true, subtree: true };
     const callback = function(mutationsList, observer) {
         for (const mutation of mutationsList) {
-            if (Array.from(mutation.addedNodes).some((node) => node.id === 'inline-dialog-repository-layout-revision-selector-dialog')) {
-                const branchSearchInput = document.getElementById('inline-dialog-repository-layout-revision-selector-dialog-search-input');
+            const branchDialogs = Array.from(mutation.addedNodes).filter((node) => branchDialogMap.has(node.id));
+            for (const branchDialog of branchDialogs) {
+                const branchSearchInput = document.getElementById(branchDialogMap.get(branchDialog.id));
                 branchSearchInput.setAttribute('autocomplete', 'off');
                 chrome.runtime.sendMessage({registerHost: true}, () => {});
             }
